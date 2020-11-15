b1d569c79e522807fa9eacc8a8b7e34e0198b01d
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2020-11-15 23:34:30 +0100

    CI: fix MIRI

--- miri_setup.sh
+++ miri_setup.sh
@@ -3,7 +3,7 @@
 MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri)
 echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
 rustup set profile minimal
-echo "$MIRI_NIGHTLY" > rust-toolchain
+rustup default "$MIRI_NIGHTLY" && echo "$MIRI_NIGHTLY" > rust-toolchain
 rustup component add miri
 
 # cargo miri test
